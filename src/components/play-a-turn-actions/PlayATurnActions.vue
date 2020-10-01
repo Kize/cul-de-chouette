e4f9f9025bef78345a1f6831d358b36263d564e7
Alban Delcourt <alban.delcourt@zenika.com>
2020-10-01 22:54:10 +0200

    fix: remove option 3 on velute action

--- src/components/play-a-turn-actions/PlayATurnActions.vue
+++ src/components/play-a-turn-actions/PlayATurnActions.vue
@@ -11,7 +11,7 @@
     <MenuAction
       label="Velute"
       :disabled="!areActionsEnabled()"
-      :options="[3, 4, 5, 6]"
+      :options="[4, 5, 6]"
       @click="basicPlay($event, playTypes.VELUTE)"
     ></MenuAction>
 
