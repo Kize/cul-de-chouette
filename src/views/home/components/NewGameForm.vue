33df31785fa0ef29ae2179b02e7c9cc70386100a
Alban Delcourt <alban.delcourt@zenika.com>
2021-07-20 18:22:48 +0200

    fix: new game form

--- src/views/home/components/NewGameForm.vue
+++ src/views/home/components/NewGameForm.vue
@@ -1,115 +1,100 @@
 <template>
   <v-form ref="formRef" v-model="isFormValid">
-    <v-card color="grey lighten-3" class="my-4" tile>
-      <v-card-title class="headline">Créer une partie</v-card-title>
-
-      <v-card-text>
-        <v-text-field
-          label="Nom de la partie"
-          v-model="form.gameName"
-          :rules="newGameNameNameRules"
-          counter
-          outlined
-          clearable
-        ></v-text-field>
-
-        <v-card>
-          <v-card-title>
-            Difficulté
-            <v-icon>mdi-star</v-icon>
-          </v-card-title>
+    <v-row>
+      <v-col md="6" cols="12">
+        <v-card color="grey lighten-3" tile>
+          <v-card-title class="headline">Créer une partie</v-card-title>
 
           <v-card-text>
-            <v-checkbox
-              dense
-              label="La soufflette"
-              v-model="form.rules.isSouffletteEnabled"
-            ></v-checkbox>
-            <v-checkbox
-              dense
-              label="Le sirop"
-              v-model="form.rules.isSiropEnabled"
-              @change="changeSiropEnabled"
-            ></v-checkbox>
-            <v-checkbox
-              dense
-              label="L'attrape-oiseau"
-              v-model="form.rules.isAttrapeOiseauEnabled"
-              :disabled="isAttrapeOiseauDisabled"
-            ></v-checkbox>
-            <v-checkbox
-              dense
-              label="Le civet"
-              v-model="form.rules.isCivetEnabled"
-              :disabled="isAttrapeOiseauDisabled"
-            ></v-checkbox>
-          </v-card-text>
-        </v-card>
-
-        <v-card>
-          <v-card-title>
-            Difficulté
-            <v-icon>mdi-star</v-icon>
-            <v-icon>mdi-star</v-icon>
-            <v-icon>mdi-star</v-icon>
-          </v-card-title>
-
-          <v-card-text>
-            <v-checkbox
-              dense
-              label="Le Bleu-Rouge"
-              v-model="form.rules.isBleuRougeEnabled"
-            ></v-checkbox>
-          </v-card-text>
-        </v-card>
-      </v-card-text>
-    </v-card>
-
-    <v-card color="grey lighten-3" class="my-4">
-      <v-card-title class="headline">
-        <span>Liste des joueurs - de 2 à 8 joueurs</span>
-      </v-card-title>
-
-      <v-card-text class="pb-0">
-        <v-row>
-          <v-col
-            lg="3"
-            md="6"
-            cols="12"
-            v-for="(_, index) of form.playerNames"
-            :key="index"
-          >
             <v-text-field
-              :label="'Joueur ' + (index + 1)"
-              v-model="form.playerNames[index]"
-              :rules="newPlayerNameRules"
-              clearable
-              outlined
+              label="Nom de la partie"
+              v-model="form.gameName"
+              :rules="newGameNameNameRules"
               counter
-              dense
-              :append-outer-icon="
-                canRemovePlayer() ? 'mdi-trash-can' : undefined
-              "
-              @click:append-outer="removePlayer(index)"
+              outlined
+              clearable
             ></v-text-field>
-          </v-col>
-        </v-row>
-      </v-card-text>
 
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn
-          class="mb-n8 mr-4"
-          color="primary"
-          fab
-          right
-          @click="addPlayer"
-          :disabled="!canAddPlayer()"
-        >
-          <v-icon>mdi-plus</v-icon>
-        </v-btn>
-      </v-card-actions>
-    </v-card>
+            <v-card class="mb-2">
+              <v-card-title>
+                Difficulté
+                <v-icon>mdi-star</v-icon>
+              </v-card-title>
+
+              <v-card-text>
+                <v-checkbox
+                  dense
+                  label="La soufflette"
+                  v-model="form.rules.isSouffletteEnabled"
+                ></v-checkbox>
+                <v-checkbox
+                  dense
+                  label="Le sirop"
+                  v-model="form.rules.isSiropEnabled"
+                  @change="changeSiropEnabled"
+                ></v-checkbox>
+                <v-checkbox
+                  dense
+                  label="L'attrape-oiseau"
+                  v-model="form.rules.isAttrapeOiseauEnabled"
+                  :disabled="isAttrapeOiseauDisabled"
+                ></v-checkbox>
+                <v-checkbox
+                  dense
+                  label="Le civet"
+                  v-model="form.rules.isCivetEnabled"
+                  :disabled="isAttrapeOiseauDisabled"
+                ></v-checkbox>
+              </v-card-text>
+            </v-card>
+
+            <v-card>
+              <v-card-title>
+                Difficulté
+                <v-icon>mdi-star</v-icon>
+                <v-icon>mdi-star</v-icon>
+                <v-icon>mdi-star</v-icon>
+              </v-card-title>
+
+              <v-card-text>
+                <v-checkbox
+                  dense
+                  label="Le Bleu-Rouge"
+                  v-model="form.rules.isBleuRougeEnabled"
+                ></v-checkbox>
+              </v-card-text>
+            </v-card>
+          </v-card-text>
+        </v-card>
+      </v-col>
+
+      <v-col md="6" cols="12">
+        <v-card color="grey lighten-3">
+          <v-card-title class="headline">
+            <span>Liste des joueurs - de 2 à 8 joueurs</span>
+          </v-card-title>
+
+          <v-card-text class="pb-0">
+            <v-row>
+              <v-col>
+                <v-combobox
+                  label="Sélection des joueurs"
+                  hint="Attention: l'ordre des joueurs est important !"
+                  v-model="form.playerNames"
+                  :items="savedPlayerNames"
+                  cache-items
+                  persistent-hint
+                  multiple
+                  deletable-chips
+                  small-chips
+                  outlined
+                ></v-combobox>
+              </v-col>
+            </v-row>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
 
     <v-row class="pt-8">
       <v-spacer></v-spacer>
@@ -134,16 +119,24 @@ import {
 } from "@/form-validation/form-validation-rules";
 import { NewGameForm } from "@/store/current-game/current-game.interface";
 
+const PLAYER_NAMES_LOCAL_STORAGE_KEY = "playerNames";
+
 @Component({})
 export default class NewGameFormSection extends Vue {
   readonly newPlayerNameRules = newPlayerNameRules;
   readonly newGameNameNameRules = newGameNameNameRules;
 
+  get savedPlayerNames(): Array<string> {
+    return JSON.parse(
+      window.localStorage.getItem(PLAYER_NAMES_LOCAL_STORAGE_KEY) || "[]"
+    );
+  }
+
   isFormValid = true;
 
   form: NewGameForm = {
     gameName: `Partie du ${new Date().toLocaleString("FR-fr").split(" à ")[0]}`,
-    playerNames: ["", "", ""],
+    playerNames: [],
     rules: {
       isSouffletteEnabled: true,
       isSiropEnabled: true,
@@ -157,20 +150,6 @@ export default class NewGameFormSection extends Vue {
     return !this.form.rules.isSiropEnabled;
   }
 
-  canRemovePlayer(): boolean {
-    return this.form.playerNames.length > 2;
-  }
-
-  canAddPlayer(): boolean {
-    return this.form.playerNames.length < 8;
-  }
-
-  removePlayer(index: number): void {
-    if (this.canRemovePlayer()) {
-      this.form.playerNames.splice(index, 1);
-    }
-  }
-
   changeSiropEnabled(newStatus: boolean): void {
     if (!newStatus) {
       this.form.rules.isAttrapeOiseauEnabled = false;
@@ -178,23 +157,25 @@ export default class NewGameFormSection extends Vue {
     }
   }
 
-  addPlayer(): void {
-    if (this.canAddPlayer()) {
-      this.form.playerNames.push("");
-    }
-  }
-
-  getList(): string {
-    return JSON.stringify(this.form.playerNames);
-  }
-
   async createGame(): Promise<void> {
     try {
       await this.$store.dispatch("currentGame/startGame", this.form);
+      this.saveCurrentPlayerNames(this.form.playerNames);
+
       await this.$router.push(ROUTES.SCRIBE_PANEL.path);
     } catch (error) {
       window.alert(error.message);
     }
+  }
+
+  saveCurrentPlayerNames(newPlayers: Array<string>): void {
+    const newPlayerNames = new Set([...newPlayers, ...this.savedPlayerNames]);
+    const sortedPlayers = [...newPlayerNames].sort();
+
+    window.localStorage.setItem(
+      PLAYER_NAMES_LOCAL_STORAGE_KEY,
+      JSON.stringify(sortedPlayers)
+    );
   }
 }
 </script>
